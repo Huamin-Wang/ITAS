f256bf102320c4199716e107ba91c6d8781dd2f6
mingo115 <mingo@xujc.com>
2024-12-02 16:35:13 +0800

    1.删除多余测试文档   2.演示利用插件着手写代码方法

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template_string,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('index.html')
+
+@app.route('/random_student')
+def random_student():
+    with open('students.txt', 'r') as file:
+        students = file.readlines()
+    student = random.choice(students).strip()
+    return render_template_string('<h1>Selected Student: {{ student }}</h1>', student=student)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
