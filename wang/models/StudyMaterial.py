f34621140993daedfcc451e2fa17ecac1b6c6c0f
Mingo <1151366039@qq.com>
2025-03-16 11:46:31 +0800

    增加学习资料模型；该quiz中时间字段为可空

---
+++ wang/models/StudyMaterial.py
@@ -0,0 +1,13 @@
+from wang.models import db
+# StudyMaterial model学习资料模型
+class StudyMaterial(db.Model):
+    __tablename__ = 'study_material'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    file = db.Column(db.String(255), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    # Relationship 可以通过课程找到学习资料，也可以通过学习资料找到课程
+    course = db.relationship('Course', backref='study_materials', lazy=True)
+    def __repr__(self):
+        return f'<StudyMaterial {self.id}>'
